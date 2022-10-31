810c53d186868100be48a7a87a5ab7136c0834cb
Jeison Primo <Jvtprimo@gmail.com>
2022-10-31 17:51:24 -0400

    Mobile Menue added

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const navbarEmail = document.querySelector(".navbar-email");
 const navbarMenuRight = document.querySelector(".desktop-menu");
+const iconTopLeft = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+
 navbarEmail.addEventListener("click", toggleMenu);
+iconTopLeft.addEventListener("click", toggleMobileMenu);
 
 function toggleMenu(){
     navbarMenuRight.classList.toggle("inactive");
-};
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
+}
